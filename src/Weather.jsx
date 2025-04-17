107e7fc41e834292b6231f3c838dd68af6bc818a
Daniel Lopes <daniel.marqueslopes74@gmail.com>
2025-04-17 21:20:47 +0200

    testing .env

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,9 +1,26 @@
-
+import { useState } from "react";
 
 function Weather(){
+
+
     return(
+        <div>
         <h1>Hello Weather App </h1>
+     
+     
+     
+     
+
+       </div>
     )
 };
+
+
+
+
+
+
+
+
 
 export default Weather
