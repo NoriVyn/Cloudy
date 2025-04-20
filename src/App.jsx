754072b82aa509cfe3fb42a69197586613a384ba
Daniel Lopes <daniel.marqueslopes74@gmail.com>
2025-04-20 23:13:04 +0200

    learning tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Weather from './Weather'
+import './Weather.css'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,52 +1,57 @@
 import { useState } from "react";
-import './Weather.css'
 
+function Weather() {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState(null);
+  const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
-function Weather(){
+  const fetchWeather = async () => {
+    if (!city) return;
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+      );
+      const data = await res.json();
+      if (data.cod === 200) {
+        setWeather(data);
+      } else {
+        alert('City not found');
+        setWeather(null);
+      }
+    } catch (err) {
+      console.error(err);
+      alert('Something happened');
+    }
+  };
 
-    const [city,setCity] = useState ('');
-    const [weather,setWeather] = useState(null);
-    const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
-   ;
+  return (
+    <div className="app flex flex-col items-center justify-center min-h-screen bg-[#9499c4]">
+      <h1 className="text-3xl font-bold text-white mb-6">Cloudy - The Weather Website, you can trust</h1>
 
-    const fetchWeather = async () => {
-        if (!city) return;
-        try {
-          const res = await fetch(
-            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-          );
-          const data = await res.json();
-          if (data.cod === 200) {
-            setWeather(data);
-          } else {
-            alert('City not found');
-            setWeather(null);
-          }
-        } catch (err) {
-          console.error(err);
-          alert('Something happened');
-        }
-      };
-    
-    return(
-        <div className="app">
-        <h1>Cloudy - The Weather Website, you can trust </h1>
-    
-        <input type="text"
+      <input
+        type="text"
         placeholder="Enter your city"
         value={city}
-        onChange={(e) => setCity(e.target.value)} />
+        onChange={(e) => setCity(e.target.value)}
+        className="w-full max-w-md p-4 bg-[#8187b8] text-white text-xl rounded-lg mb-6 focus:outline-none"
+      />
 
-        <button onClick={fetchWeather}>Search</button>
-        
-        {weather && (
-        <div className="weather-card">
-            <h2>{weather.name}</h2>
-        <p>{weather.weather[0].description}</p>
-          <p>🌡️ {Math.round(weather.main.temp)}°C</p>
+      <button
+        onClick={fetchWeather}
+        className="bg-sky-500/50 text-white px-6 py-3 rounded-lg hover:bg-[#42423e] transition-all"
+      >
+        Search
+      </button>
+
+      {weather && (
+        <div className="weather-card mt-16 mb-8 bg-[#f0f0f0a1] p-16 rounded-3xl inline-block max-w-md ">
+          <h2 className="text-2xl font-semibold text-center">{weather.name}</h2>
+          <p className="text-center">{weather.weather[0].description}</p>
+          <p className="text-center">{Math.round(weather.main.temp)}°C</p>
           <img
             src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
             alt="weather icon"
+            className="mx-auto my-4 text-center"
           />
           <p>{weather.main.humidity}% humidity in the air</p>
         </div>
@@ -55,13 +60,4 @@ function Weather(){
   );
 }
 
-
-
-
-
-
-
-
-
-
-export default Weather
+export default Weather;
